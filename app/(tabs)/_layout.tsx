c0534cdf7ba637e5c918549cc171ba3e6979f999
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-01-17 21:23:54 +0800

    refactor: added tabs for the project

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,64 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    // create a tab navigation containing this tabs
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'coral' }}>
-      <Tabs.Screen name="index" options={{
-        title: "Home", tabBarIcon: ({ color, focused }) => {
-          return focused ? (
-            <FontAwesome name="home" size={24} color={color} />
-          ) : (
-            <FontAwesome name="home" size={24} color="black" />) //make the icon adopt the color tint by using destructuring
-        }
-      }} />
+    <Tabs
+      screenOptions={{
+        headerStyle: { backgroundColor: "#f5f5f5" },
+        headerShadowVisible: false,
+        headerTitleAlign: "center",
+        headerTitleStyle: { fontWeight: "bold", fontSize: 18 },
+        tabBarStyle: {
+          backgroundColor: "#f5f5f5",
+          borderTopWidth: 0,
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+        tabBarActiveTintColor: "#6200ee",
+        tabBarInactiveTintColor: "#666666",
+
+      }}
+      >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Today's Habits",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="calendar-today"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="streaks"
+        options={{
+          title: "Streaks",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="chart-line"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="add-habit"
+        options={{
+          title: "Add Habit",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="plus-circle"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
-  ) //shared ui elements in different routes like headers, tabs
+  );
 }
